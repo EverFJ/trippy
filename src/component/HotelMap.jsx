f6ad8b79ab4b55e3550a89dc450c4f69b7977346
Francois Janson <jansonfrancois@gmail.com>
2021-09-15 15:32:26 +0200

    navbar Component

--- src/component/HotelMap.jsx
+++ src/component/HotelMap.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class HotelMap extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>HotelMap</h1>
+      </div>
+    );
+  }
 }
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class NavBar extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
-          </a>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="#">
+            Trippy
+          </Link>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
@@ -17,27 +18,29 @@ export default class NavBar extends Component {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Hotels
+                </Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
-                </a>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Restaurants
+                </Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Login
+                </Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Signup
+                </Link>
               </li>
             </ul>
           </div>
