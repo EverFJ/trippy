93b3b47d85775e0628c79cd0569a9459480c3237
Francois Janson <jansonfrancois@gmail.com>
2021-09-15 15:23:02 +0200

    adding router logic

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import NavBar from "./component/NavBar";
+import Home from "./component/Home";
+import Hotels from "./component/Hotels";
+import HotelPage from "./component/HotelPage";
+import Error404 from "./component/Error404";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/hotels/:city" component={Hotels} />
+        <Route path="hotel/:id" component={HotelPage} />
+        <Route path="*" component={Error404} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
