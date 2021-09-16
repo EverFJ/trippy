bf46ffffc866891e1c90db261336906161474f37
Francois Janson <jansonfrancois@gmail.com>
2021-09-16 14:36:16 +0200

    fix merge

--- src/component/CityCard.jsx
+++ src/component/CityCard.jsx
@@ -5,14 +5,10 @@ export default class CityCard extends Component {
   render() {
     const { name, slug, source } = this.props;
     return (
-<<<<<<< HEAD
-      <div className={name==="Paris"? "card big"  : "card"} style={{width:"18rem", height:"20rem"}} >
-=======
       <div
-        className={name === "Paris" ? "card big m-4" : "card"}
-        style={{ width: "10rem" }}
+        className={name === "Paris" ? "card big" : "card"}
+        style={{ width: "18rem", height: "20rem" }}
       >
->>>>>>> c610da7711cb56d0f3d7bcd8f9eb4b285aff5199
         <p>{name}</p>
         <Link to={`/hotels/${slug}`}>
           <img
@@ -22,13 +18,8 @@ export default class CityCard extends Component {
                 : `http://via.placeholder.com/300x200`
             }
             alt="city image"
-<<<<<<< HEAD
-
-            style={{width:"17rem"}}
-        />
-=======
+            style={{ width: "17rem" }}
           />
->>>>>>> c610da7711cb56d0f3d7bcd8f9eb4b285aff5199
         </Link>
       </div>
     );
