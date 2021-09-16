c610da7711cb56d0f3d7bcd8f9eb4b285aff5199
Francois Janson <jansonfrancois@gmail.com>
2021-09-16 14:31:02 +0200

    fix merge

--- src/component/CityCard.jsx
+++ src/component/CityCard.jsx
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-
 export default class CityCard extends Component {
   render() {
     const { name, slug, source } = this.props;
     return (
-      <div className={name==="Paris"? "card big m-4"  : "card"} style={{width:"10rem"}}>
+      <div
+        className={name === "Paris" ? "card big m-4" : "card"}
+        style={{ width: "10rem" }}
+      >
         <p>{name}</p>
         <Link to={`/hotels/${slug}`}>
-<<<<<<< HEAD
-          <img src={`http://localhost:3002/${source}`} style={{width: "150px"}} alt="city image" />
-=======
           <img
             src={
               source
@@ -20,7 +19,6 @@ export default class CityCard extends Component {
             }
             alt="city image"
           />
->>>>>>> 82a4fd0a44047dedece93f5ae81458b1866ab3cb
         </Link>
       </div>
     );
