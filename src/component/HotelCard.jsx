d93cf9219e43d2e2ca8fb955a7e0ac700a1f62ef
Semou <semou.ach@gmail.com>
2021-09-16 16:34:11 +0200

    info map

--- src/component/HotelCard.jsx
+++ src/component/HotelCard.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 export default class HotelCard extends Component {
+
+
   render() {
     console.log(this.props.hotel);
+    console.log(this.props.hotel.name);
     return (
-      <div>
-        <h1>HotelCard</h1>
-      </div>
+      <>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={"`http://localhost:3002/" + this.props.hotel.pictures[0]} class="card-img-top" alt="..." />
+          <div className="card-body">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">{this.props.hotel.address}</li>
+              <li class="list-group-item">{this.props.hotel.name}</li>
+              <li class="list-group-item">{this.props.hotel.stars}</li>
+            </ul>
+          </div>
+        </div>
+      </>
     );
   }
 }
