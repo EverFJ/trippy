276775805397a3e781b6d92465244dbf919a1dee
Francois Janson <jansonfrancois@gmail.com>
2021-09-16 14:28:09 +0200

    creating Hotels component

--- src/component/Hotels.jsx
+++ src/component/Hotels.jsx
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
 
 export default class Hotels extends Component {
-  constructor(){
-    super()
-    this.state={
-      Hotels:[]
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      hotels: [],
+    };
   }
 
-  componentDidMount(){
-    fetch("http://localhost:3002/api/hotels/city/<city>")
-    .then()
-    .then()
+  componentDidMount() {
+    fetch(
+      `http://localhost:3002/api/hotels/city/${this.props.match.params.city}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("fetch data : ", data);
+      })
+      .catch((err) => console.error(err));
   }
 
   render() {
-
+    // console.log(this.props.match.params.city);
     return (
       <>
         <h1>Hotels</h1>
-
       </>
     );
   }
